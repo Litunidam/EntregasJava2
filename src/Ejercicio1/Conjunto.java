c435f7d01345f325d0492c5b1ea01119f4bc3d54
Manuel Martín Benito <manuelmarb@gmail.com>
2020-02-13 23:51:37 +0100

    planteamiento del ejercicio 1 wtf

--- src/Ejercicio1/Conjunto.java
+++ src/Ejercicio1/Conjunto.java
@@ -1,5 +1,26 @@
 package Ejercicio1;
 
+import java.util.ArrayList;
+
 public class Conjunto {
+	Conjunto c = new Conjunto();
+	private ArrayList<Integer> l=new ArrayList<Integer>();
+	
+	public void setElemento(int n) {
+		
+		if (l.contains(n)) {
+			System.out.println("No se pueden añadir dos elementos iguales");
+		}else {
+			l.add(n);
+		}
+	}
+	public void deleteElemento(int n) {
+		
+		if (l.contains(n)) {
+			l.remove(n);
+		}else {
+			System.out.println("No está presente el elemento "+n+" en la lista");
+		}
+	}
 
 }
