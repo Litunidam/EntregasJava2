957109e439a715bfacf0c54dba8f0a363cf0b1ae
litunidam <manuelmarb@gmail.com>
2020-03-03 18:33:53 +0100

    Medio hecho ejercicio 2

---
+++ src/ejercicio_2/Principal.java
@@ -0,0 +1,11 @@
+package ejercicio_2;
+
+public class Principal {
+	public static void main(String[] args) {
+	
+		Vector3D v = new Vector3D(1,2,3);
+		
+		System.out.println(v.moduloVector());
+		
+	}
+}
---
+++ src/ejercicio_2/Vector3D.java
@@ -0,0 +1,44 @@
+package ejercicio_2;
+
+public class Vector3D {
+
+	private int [] v = new int [3];
+	
+	public Vector3D() {
+		
+	}
+	
+	public Vector3D(int a, int b, int c) {
+		this.v[0]=a;
+		this.v[1]=b;
+		this.v[2]=c;
+	}
+	
+	public Vector3D sumaVector(Vector3D ve) {
+		
+		Vector3D aux = new Vector3D();
+		aux.v[0]=this.v[0]+ve.v[0];
+		aux.v[1]=this.v[1]+ve.v[1];
+		aux.v[2]=this.v[2]+ve.v[2];
+
+		return aux;
+	}
+	
+	public Vector3D restaVector(Vector3D ve) {
+
+		Vector3D aux = new Vector3D();
+		aux.v[0]=this.v[0]-ve.v[0];
+		aux.v[1]=this.v[1]-ve.v[1];
+		aux.v[2]=this.v[2]-ve.v[2];
+
+		return aux;
+	}
+	
+	public double moduloVector() {
+		
+		double modulo;
+		modulo = Math.sqrt(Math.pow(this.v[0],2)+Math.pow(this.v[1],2)+Math.pow(this.v[2],2));
+		
+		return modulo;
+	}
+}
