d5d1f8bc73183d33187e1b516a213ccfc99dec7e
litunidam <manuelmarb@gmail.com>
2020-03-04 20:37:02 +0100

    Inicio del tablero

--- src/ejercicio_3/Tablero.java
+++ src/ejercicio_3/Tablero.java
@@ -1,17 +1,32 @@
 package ejercicio_3;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
+
 import javax.swing.*;
 
 public class Tablero extends JFrame {
 
 	public Tablero() {
-	
-		this.setSize(600,400);
+		
+		setBounds(500, 100, 600, 400);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
 	}
 	private String[][] tablero=new String[3][3];
 	
+}
 
-
+class Lamina extends JPanel{
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D)g; 
+		
+		Line2D line; 
+	}
+	
+	
 }
