a01663ebefe90203668e4b9939069931f0f856e3
Manuel <manuelmarb@gmail.com>
2020-03-04 13:32:11 +0100

    Ventana hecha, falta dibujar

---
+++ src/ejercicio_3/Principal.java
@@ -0,0 +1,13 @@
+package ejercicio_3;
+
+public class Principal {
+
+	public static void main(String []args) {
+		
+		Tablero tablero = new Tablero();
+		
+		tablero.setVisible(true);
+		
+		
+	}
+}
---
+++ src/ejercicio_3/Tablero.java
@@ -0,0 +1,17 @@
+package ejercicio_3;
+
+import javax.swing.*;
+
+public class Tablero extends JFrame {
+
+	public Tablero() {
+	
+		this.setSize(600,400);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	}
+	private String[][] tablero=new String[3][3];
+	
+
+
+}
